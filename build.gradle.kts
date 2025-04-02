909727a4aed82d8eb865b1f0ff7eecf1b6d8ee12
Colin Fleming <colin@colinfleming.net>
2025-04-02 18:16:50 +1300

    Update to 2025.1 EAP, and update Kotlin and IntelliJ Gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.9.25"
-  id("org.jetbrains.intellij.platform") version "2.3.0"
+  id("org.jetbrains.kotlin.jvm") version "2.1.10"
+  id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
 group = "ideogram"
@@ -17,10 +17,12 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
+
   intellijPlatform {
-    create("IC", "2024.2.5")
+    create("IC", "251.23774.109-EAP-SNAPSHOT", useInstaller = false)
     testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
+    jetbrainsRuntime()
     // Add necessary plugin dependencies for compilation here, example:
     // bundledPlugin("com.intellij.java")
   }
@@ -29,7 +31,7 @@ dependencies {
 intellijPlatform {
   pluginConfiguration {
     ideaVersion {
-      sinceBuild = "242"
+      sinceBuild = "251"
     }
 
     changeNotes = """
